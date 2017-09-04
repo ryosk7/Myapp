a9089b10e4280ac86870263da121af09a98af4e6
Ryosuke Uchida <uchiryo7@gmail.com>
2017-09-05 08:28:36 +0900

    full_tilte helperを追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Seed and Growth"
+    assert_select "title", "Seed and Growth"
   end
 
   test "should get about" do
