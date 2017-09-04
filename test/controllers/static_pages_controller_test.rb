fc1aa3b906d98426834b8840e0db48672e542d42
Ryosuke Uchida <uchiryo7@gmail.com>
2017-09-05 04:59:50 +0900

    関連するタイトルを追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Seed and Growth"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Seed and Growth"
   end
 
 end
