ab4de259dc3015e209fd929064d6c831d43decf1
Ryosuke Uchida <uchiryo7@gmail.com>
2017-09-06 16:27:10 +0900

    welcome pageの削除とlayoutの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
 
-  get 'welcome/index'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
-  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
