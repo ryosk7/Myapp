6f4f95c66b9a5181577940fd52240dc91b0bb429
Ryosuke Uchida <uchiryo7@gmail.com>
2017-09-07 17:27:33 +0900

    User controllerを削除し Users controllerを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
-class UserController < ApplicationController
-
+class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get signup_path
+    get users_new_url
     assert_response :success
   end
 
